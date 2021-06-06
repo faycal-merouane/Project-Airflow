fec5b14cf3f23dbc298ef356f4e186bc271f5ab7
faycal-merouane <faycal.merouanee@gmail.com>
2021-06-06 12:20:40 +0100

    first commit

--- plugins/operators/stage_redshift.py
+++ plugins/operators/stage_redshift.py
@@ -1,10 +1,10 @@
-from airflow.contrib.hooks.aws_hook import AwsHook
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
+from airflow.contrib.hooks.aws_hook import AwsHook
 
-
-class S3ToRedshiftOperator(BaseOperator):
+class StageToRedshiftOperator(BaseOperator):
+    ui_color = '#358140'
     template_fields = ("s3_key",)
     copy_sql = """
         COPY {}
@@ -12,9 +12,8 @@ class S3ToRedshiftOperator(BaseOperator):
         ACCESS_KEY_ID '{}'
         SECRET_ACCESS_KEY '{}'
         IGNOREHEADER {}
-        DELIMITER '{}'
+        JSON 'auto' region 'us-west-2'
     """
-
 
     @apply_defaults
     def __init__(self,
@@ -23,22 +22,22 @@ class S3ToRedshiftOperator(BaseOperator):
                  table="",
                  s3_bucket="",
                  s3_key="",
-                 delimiter=",",
-                 ignore_headers=1,
+                 ignore_headers= True,
                  *args, **kwargs):
 
-        super(S3ToRedshiftOperator, self).__init__(*args, **kwargs)
-        self.table = table
+        super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
         self.redshift_conn_id = redshift_conn_id
+        self.aws_credentials_id = aws_credentials_id
+        self.table = table
         self.s3_bucket = s3_bucket
         self.s3_key = s3_key
-        self.delimiter = delimiter
         self.ignore_headers = ignore_headers
-        self.aws_credentials_id = aws_credentials_id
 
     def execute(self, context):
+        self.log.info('StageToRedshiftOperator not implemented yet')
         aws_hook = AwsHook(self.aws_credentials_id)
         credentials = aws_hook.get_credentials()
+
         redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
 
         self.log.info("Clearing data from destination Redshift table")
@@ -46,13 +45,21 @@ class S3ToRedshiftOperator(BaseOperator):
 
         self.log.info("Copying data from S3 to Redshift")
         rendered_key = self.s3_key.format(**context)
+
+        #test print 
+        self.log.info(f"faycal udacity test : renderd_key = {rendered_key}, s3_key = {self.s3_key},context = {context}")
+        
+        
         s3_path = "s3://{}/{}".format(self.s3_bucket, rendered_key)
-        formatted_sql = S3ToRedshiftOperator.copy_sql.format(
+        formatted_sql = self.copy_sql.format(
             self.table,
             s3_path,
             credentials.access_key,
             credentials.secret_key,
-            self.ignore_headers,
-            self.delimiter
+            self.ignore_headers
         )
         redshift.run(formatted_sql)
+
+
+
+

7f1c1b4c5862d1006738461e80c68b33369f1540
faycal-merouane <faycal.merouanee@gmail.com>
2021-06-06 14:35:58 +0100

    add data quality fct

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -8,15 +8,22 @@ class DataQualityOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 checks_dic = [{"check_sql":"", "check_expected":""}],
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.checks_dic = checks_dic
 
     def execute(self, context):
+        error_count = 0
         self.log.info('DataQualityOperator not implemented yet')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        for ch in self.checks_dic:
+            result = redshift.get_records(ch.get("check_sql"))[0]
+            if(result == ch.get("check_expected")):
+                error_count += 1
+                self.log.error("test for {} \n faill with result  =  {} expect result is {}".format(ch.get("check_sql"), result, ch.get("check_expected")))
+        if(error_count > 0): 
+            raise ValueError('Data quality check failed')
